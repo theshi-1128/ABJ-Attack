784cdf743c923286e61785ff91d4c18cca437c04
theshi <84408473+theshi-1128@users.noreply.github.com>
2025-05-19 17:36:12 +0800

    Update toxicity_adjustment.py

--- pipeline/toxicity_adjustment.py
+++ pipeline/toxicity_adjustment.py
@@ -9,12 +9,11 @@ def toxicity_adjustment(response, data, judge_model, assist_model):
     judgment = clean_text(judge_model.generate_response(detailed_judge_prompt.format(PROMPT=response)))
     print_judgment(judgment)
     # toxicity enhancement
-    if judgment == '0':
+    if '1' not in judgment:
         new_json_data = assist_model.generate_response(toxicity_enhancement.format(DATA=data))
     # toxicity reduction
-    elif judgment == '1':
+    elif '1' in judgment:
         new_json_data = assist_model.generate_response(toxicity_reduction.format(DATA=data))
     new_data = format_json_to_data(new_json_data)
     return new_data
-
 
