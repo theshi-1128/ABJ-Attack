d65b4c45742d771c6c44212f65b82cb53899a3bc
theshi <84408473+theshi-1128@users.noreply.github.com>
2025-05-27 11:27:43 +0800

    Add files via upload

---
+++ api_config.py
@@ -0,0 +1,42 @@
+model_list = {
+    "gpt4o": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'gpt-4o-2024-11-20'
+    },
+    "gpt4o_vl": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'gpt-4o-2024-11-20'
+    },
+    "claude3_haiku": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'claude-3-haiku-20240307'
+    },
+    "deepseek_v3": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'deepseek-chat'
+    },
+    "glm4": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'THUDM/GLM-4-9B-0414'
+    },
+    "o1": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'o1-preview-2024-09-12'
+    },
+    "r1": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'deepseek-reasoner'
+    },
+    "gpt_img": {
+        'api_key': 'replace with your api_key',
+        'base_url': "replace with your base_url",
+        'model_name': 'gpt-image-1'
+    }
+}
